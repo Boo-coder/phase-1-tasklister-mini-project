b028e277e531224a276ba0912c2c2711871c44f3
Boo-coder <Bouna112@yahoo.com>
2025-03-09 21:58:26 -0400

    mprj

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form").addEventListener("submit", (e) => {
       e.preventDefault()
+      
+      
+      const taskDescription = document.getElementById("new-task-description").value
+      
+      console.log(taskDescription)
+
+      const taskList = document.getElementById("tasks")
+      const newTask = document.createElement("li")
+      newTask.textContent = taskDescription
+      taskList.appendChild(newTask)
   })
 })
